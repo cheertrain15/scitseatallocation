23c5bfc5a725c5c2ff8a3f70e25b805d9cf7b96c
hyeinpark <hyeinpark@192.168.0.4>
2018-04-01 13:42:16 +0900

    surveyCreate 2018.04.01 박혜인

--- SCITPortalSystem/src/main/webapp/WEB-INF/views/survey/surveyCreate.jsp
+++ SCITPortalSystem/src/main/webapp/WEB-INF/views/survey/surveyCreate.jsp
@@ -14,9 +14,16 @@
 <h1>새 설문조사 작성</h1>
 <div class="toolBar">
 
-<div class="tools" id="text"><p>텍스트툴</p></div>
-<div class="tools" id="radiogroup"><p>라디오그룹툴</p></div>
-<div class="tools" id="checkbox"><p>체크박스툴</p></div>
+<div class="tools" id="text">텍스트툴</div>
+<div class="tools" id="radiogroup">라디오그룹툴</div>
+<div class="tools" id="checkbox">체크박스툴</div>
+
+
+<!-- <ul>
+<li class="tools" id="text">텍스트툴</li>
+<li class="tools" id="radiogroup">라디오그룹툴</li>
+<li class="tools" id="checkbox">체크박스툴</li>
+</ul> -->
 
 </div>
 
@@ -25,7 +32,12 @@
 </div>
 
 <div class="surveyCanvas">
-<p>여기에 툴을 drag & drop하세요</p>
+<p>여기에 tool 드랍하셈~</p>
+<div class="questions" id="question">설문 조사 리스트 예시</div>
+<!-- <ul id="surveyList">
+<li id="question">설문조사 내용 예시 1</li>
+</ul> -->
+
 </div>
 
 <div class="editSurvey">
--- SCITPortalSystem/src/main/webapp/resources/js/survey/surveyCreate.js
+++ SCITPortalSystem/src/main/webapp/resources/js/survey/surveyCreate.js
@@ -5,5 +5,43 @@
 // drag
 
 $( function() {
-    $( "#text" ).draggable();
-  } );
+	
+	$( "#surveyList" ).sortable({
+	      revert: true
+	    });
+	
+    $( "#text, #radiogroup, #checkbox").draggable(
+    
+    		{ scroll: true },
+    		{ cursor: "move" },
+    		{ 
+    			connectToSortable: "#surveyList",
+    		    helper: "clone",
+    		    revert: "invalid"}
+    		
+    );
+
+    $( "ul, li" ).disableSelection();
+    
+	var question = 1;
+	var str = "";
+    
+    $( ".surveyCanvas" ).droppable({
+        drop: function( event, ui ) {
+        	
+        	str += "<div class='questions' id='question" + question + "'>"
+        		+ "질문" + question + "."
+        		+ "<input type=text placeholder='질문내용을 입력하세요'>"
+        		+"</div>";
+        		
+          $( this )
+            .find( "p" )
+              .html( "드랍되었음당" )
+              
+          $(event.target).html(str);
+          
+          question++;
+        }
+      });
+   
+} );
