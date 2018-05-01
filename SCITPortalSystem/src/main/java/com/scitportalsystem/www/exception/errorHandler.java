d1b31de421ae0d8c232ed3b6480846ba5eee1dee
SCITMASTER <SCITMASTER@LAPTOP-7KBCOPLN>
2018-05-01 16:50:49 +0900

    2018-05-01 지현우
    에러처리 추가

--- SCITPortalSystem/src/main/java/com/scitportalsystem/www/exception/errorHandler.java
+++ SCITPortalSystem/src/main/java/com/scitportalsystem/www/exception/errorHandler.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-//@ControllerAdvice
-//public class errorHandler extends HandlerInterceptorAdapter{
-//
-//	@ExceptionHandler(Exception.class)
-//	public String errorHandling(Exception e){
-//		e.printStackTrace();
-//		return "error";
-//	}
-//}
+@ControllerAdvice
+public class errorHandler extends HandlerInterceptorAdapter{
+
+	@ExceptionHandler(Exception.class)
+	public String errorHandling(Exception e){
+		e.printStackTrace();
+		return "/error";
+	}
+}
